6499ce28a37e4dd63ce9d58335937ad4dd5d346f
Eric Kao <ucberickao@gmail.com>
2020-10-14 11:43:00 -0700

    server routes and readme

--- server/index.js
+++ server/index.js
@@ -8,20 +8,29 @@ const port = 1234;
 
 app.use(express.static(path.join(__dirname, '/../public/dist')));
 
+app.get('/:pid/recommendation/getInfo', (req, res) => {
+  getInfo(req.params.pid, (data) => {
+    const related = data[0].related_pid;
+    getInfo(related, (eachData) => {
+      res.status(200).send(eachData);
+    });
+  });
+});
+
 app.get('/:pid', (req, res) => {
   res.status(200).send('get');
 });
 
-app.post('/:pid', (req, res) => {
-  res.status(200).send('post');
+app.post('/', (req, res) => {
+  res.status(201).send('post');
 });
 
 app.put('/:pid', (req, res) => {
-  res.status(200).send('put');
+  res.status(204).send('put');
 });
 
 app.delete('/:pid', (req, res) => {
-  res.status(200).send('delete');
+  res.status(204).send('delete');
 });
 
 app.listen(port, () => {
