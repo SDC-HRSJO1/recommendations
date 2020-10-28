1512ccff5c1efe9aebd2b67bfe71fe65ad91aec6
Eric Kao <ucberickao@gmail.com>
2020-10-27 23:54:42 -0700

    query update

--- database/index.js
+++ database/index.js
@@ -5,7 +5,7 @@ const { Pool } = require('pg');
 const pool = new Pool({ database: 'products' });
 
 const getRecs = (pid, callback) => {
-  pool.query(`select * from info t1 inner join info t2 on t1.id=${pid} and t2.id!=${pid} and t1.department=t2.department and t1.category=t2.category and t1.subcategory=t2.subcategory and t1.brand=t2.brand`)
+  pool.query(`select t2.* from info t1 inner join info t2 on t1.id=${pid} and t2.id!=${pid} and t1.department=t2.department and t1.category=t2.category and t1.subcategory=t2.subcategory and t1.brand=t2.brand`)
     .then(rec => { callback(null, rec.rows); })
     .catch(() => { callback('db'); });
 };
